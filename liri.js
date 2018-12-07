c65a119dbe2acd025984bbd774e3c222735b6784
Katerine <katerinehw@att.net>
2018-12-06 16:35:43 -0800

    updated code

--- liri.js
+++ liri.js
@@ -1,10 +1,13 @@
 // node mod imports needed:
 var dot = require("dotenv").config();
 var keys = require("./keys.js");
-var music = require("node-spotify-api");
-var band = require("");
-var movie = require("");
+var Spotify = require("node-spotify-api");
+var request = require("console.table");
 var moment = require("moment");
+var cTable = require("console.table");
+var fs =require("fs");
+
+
 var spotify = new Spotify({
     id: keys.spotify.id,
     secret: keys.spotify.secret
@@ -16,17 +19,6 @@ var spotify = new Spotify({
 // functions: Make it so liri.js can take in one of the following commands:
 
 //    * `concert-this`
-// `node liri.js concert-this <artist/band name here>`
-
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-//      * Name of the venue
-
-//      * Venue location
-
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-//    * `spotify-this-song`
-
 if (process.argv[2] === "concert this") {
     console.log(artist);
     var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -39,7 +31,7 @@ if (process.argv[2] === "concert this") {
     });
 
 
-} else if (process.argv[2] == 'spotify-this-song') {
+} else if (process.argv[2] == "spotify-this-song") {
 
     var songName = process.argv.slice(3).join(" ");
 
@@ -48,9 +40,9 @@ if (process.argv[2] === "concert this") {
     }
 
 
-    spotify.search({ type: 'track', query: songName, limit: 10 }, function (err, data) {
+    spotify.search({ type: "track", query: songName, limit: 10 }, function (err, data) {
         if (err) {
-            return console.log('Error occurred: ' + err);
+            return console.log("Error occurred: " + err);
         }
 
         var tableArray = [];
@@ -72,6 +64,7 @@ if (process.argv[2] === "concert this") {
 
 
     });
+    //    * `movie-this`
 } else if (process.argv[2] == 'movie-this') {
     var movieName = process.argv.slice(3).join(" ");
 
@@ -91,55 +84,13 @@ if (process.argv[2] === "concert this") {
         console.log("Actors :" +  result.Actors);
 
     });
-
-} else if ( process.argv[2] == 'do-what-it-says') {
-    console.log('do what it says')
+//    * `do-what-it-says'
+} else if ( process.argv[2] == "do-what-it-says") {
+    console.log("do what it says")
 }
-    // `node liri.js spotify-this-song '<song name here>'`
+    
 
-// * This will show the following information about the song in your terminal/bash window
 
-//   * Artist(s)
 
-//   * The song's name
 
-//   * A preview link of the song from Spotify
-
-//   * The album that the song is from
-
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-//    * `movie-this`
-// `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Rotten Tomatoes Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-// * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-// * It's on Netflix
-
-//    * `do-what-it-says`
-// `node liri.js do-what-it-says`
-
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
